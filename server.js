f2c17d509e2a56714bfbeceb9fd66c81d51a0adb
Carter_Schmitz <carterschmitz95@gmail.com>
2023-01-01 20:25:38 -0600

    added function to start server, and post a new note

--- server.js
+++ server.js
@@ -14,15 +14,41 @@ let notes = require("./db/db.json");
 // Routes
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
-  });
-  
-  // Display notes
-  app.get("/api/notes", function (req, res) {
+});
+
+// Display notes
+app.get("/api/notes", function (req, res) {
     fs.readFile("db/db.json", "utf8", function (err, data) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      res.json(notes);
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.json(notes);
     });
-  });
+});
+
+// Starts server to begin listening
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
+// Create new note
+app.post("/api/notes", function (req, res) {
+    let randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    let id = randLetter + Date.now();
+    let newNote = {
+        id: id,
+        title: req.body.title,
+        text: req.body.text,
+    };
+    console.log(typeof notes);
+    notes.push(newNote);
+    const stringifyNote = JSON.stringify(notes);
+    res.json(notes);
+    fs.writeFile("db/db.json", stringifyNote, (err) => {
+        if (err) console.log(err);
+        else {
+            console.log("Note successfully saved to db.json");
+        }
+    });
+});
