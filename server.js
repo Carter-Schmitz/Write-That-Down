ea9245cbc77c658ea52cd5262c9b7de5864ce219
Carter_Schmitz <carterschmitz95@gmail.com>
2023-01-01 20:23:32 -0600

    set up express middleware

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+// Set up Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+let notes = require("./db/db.json");
